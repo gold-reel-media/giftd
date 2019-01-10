caf2dd41393393d240daba81b479f4ef4d036b1a
Meg <m.a.gallagher09@gmail.com>
2019-01-10 16:28:18 -0600

    adding auth0

--- giftd/src/App.js
+++ giftd/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App blah blah blah">
+      <div className="App">
         <Header />
         <Main />
       </div>
