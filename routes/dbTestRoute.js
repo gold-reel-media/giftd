22c2271ef077d50602eb808e34716648ad1836f8
TomSmaj <thomas.smajstrla@gmail.com>
2019-01-19 16:49:16 -0600

    get serviceWorker on github

--- routes/dbTestRoute.js
+++ routes/dbTestRoute.js
@@ -29,11 +29,11 @@ module.exports = function (app) {
     });
 
     app.post("/dbrelate", (req, res) => {
-        db.Item.find({ where: { name: 'testitem' } }).then(item => {
-            db.Wishlist.find({ where: { wishlistId: 1 } }).then(wishlist => {
-                item.addWishlists([wishlist]);
-            });
-        });
+        // db.Item.find({ where: { name: 'testitem' } }).then(item => {
+        //     db.Wishlist.find({ where: { wishlistId: 1 } }).then(wishlist => {
+        //         item.addWishlists([wishlist]);
+        //     });
+        // });
         db.User.find({ where: { username: 'Bob' } }).then(friend1 => {
             db.User.find({ where: { username: "Larry" } }).then(friend2 => {
                 friend1.addFriend2([friend2]);
@@ -41,13 +41,20 @@ module.exports = function (app) {
         });
     });
 
-    // app.get("/friends/:user", (req, res) => {
-    //     db.User.find( { where: {username: req.params.user}}).then(dbUser => {
-    //         res.json(dbUser.getFriend2());
-    //     })
-    // });
+    app.post("/addItem", (req, res) => {
+        db.Item.find({ where: { itemId: 1 } }).then(item => {
+            db.Wishlist.find({ where: { wishlistId: 1 } }).then(wishlist => {
+                wishlist.addItems([item]);
+            });
+        });
+    });
 
     app.get("/friends/:user", (req, res) => {
-        db.User.find({ where: {username: req.params.user} }).then(guy => guy.getFriend2().then(friendos => res.json(friendos)))
+        db.User.find({ where: {username: req.params.user} }).then( friends => friends.getFriend2().then(friend => res.json(friend)))
     });
+
+    app.get("/wishlist/:wish", (req, res) => {
+        db.Wishlist.find({ where: {name: req.params.wish} }).then( wishlist => wishlist.getItems().then(items => res.json(items)))
+    });
+
 };
