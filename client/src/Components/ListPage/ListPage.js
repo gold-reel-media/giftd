b46fd788085a1d996d3dce562bbbc8ea41fd8e55
megzimo <40956115+megzimo@users.noreply.github.com>
2019-01-26 11:08:11 -0600

    staged changes

--- client/src/Components/ListPage/ListPage.js
+++ client/src/Components/ListPage/ListPage.js
@@ -1,37 +1,39 @@
-import React, { Component } from 'react';
-import AddItemForm from '../AddItemForm/AddItemForm';
-import Items from '../Items/Items';
+import React, { Component } from "react";
+import AddItemForm from "../AddItemForm/AddItemForm";
+import NavBar from "../../NavBar/NavBar";
+import Items from "../Items/Items";
 
 class ListPage extends Component {
-    state = {
-        items: {}
-    };
+  state = {
+    items: {}
+  };
 
-    addItem = item => {
-        //copy of existing state
-        const items = {...this.state.items};
-        //add new item to items
-         items[`item`] = item;
-         //set new items object to state
-         this.setState({
-             items
-         })
-    }
-    render() {
-        return (
-            <div className="list-page-container">
-                <h2>List Page</h2>
-                <AddItemForm addItem={this.addItem}/>
-                <ul className="lists">
-                    {Object.keys(this.state.items).map( key => 
-                    
-                    <Items key={key} details={this.state.items}/>
-                  
-                  )}
-                </ul>
-            </div>
-        );
-    }
+  addItem = item => {
+    //copy of existing state
+    const items = { ...this.state.items };
+    //add new item to items
+    items[`item`] = item;
+    //set new items object to state
+    this.setState({
+      items
+    });
+  };
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="list-page-container">
+          <h2>List Page</h2>
+          <AddItemForm addItem={this.addItem} />
+          <ul className="lists">
+            {Object.keys(this.state.items).map(key => (
+              <Items key={key} details={this.state.items} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ListPage;
