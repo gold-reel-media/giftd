0144f670b2daba9b982f413bbcfd91fd12a79e66
ckernan <41130164+ckernan@users.noreply.github.com>
2019-01-25 21:41:53 -0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
-    password: "MyNewPass",
+    password: "root",
     database: "giftd_db",
     host: "localhost",
     dialect: "mysql"
