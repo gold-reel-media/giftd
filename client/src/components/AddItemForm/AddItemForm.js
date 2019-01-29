bf4ed879f9a08ef1766d2f54cb8484a34c95da7a
Cristina Kernan <41130164+ckernan@users.noreply.github.com>
2019-01-29 15:33:33 -0600

    more button styling

--- client/src/components/AddItemForm/AddItemForm.js
+++ client/src/components/AddItemForm/AddItemForm.js
@@ -35,6 +35,15 @@ const styles = theme => ({
  
 });
 
+const button = {
+  color: "#0d9aaa",
+  border: "1px solid #0d9aaa",
+}
+
+// const dialog = {
+//   width: "600px"
+// }
+
 function Transition(props) {
   return <Slide direction="up" {...props} />;
 }
@@ -261,7 +270,8 @@ class OutlinedTextFields extends React.Component {
                   <ListItem key={item.itemId}>
                     <div className="item-popup">
                       <Button
-                        className
+                        className="list-item-btn"
+                        style={button}
                         name={item.name}
                         price={item.price}
                         itemLink={item.itemLink}
@@ -273,6 +283,7 @@ class OutlinedTextFields extends React.Component {
                         <strong>{item.name}</strong>
                       </Button>
                       <Dialog
+                        
                         open={this.state.open}
                         TransitionComponent={Transition}
                         keepMounted
@@ -332,44 +343,3 @@ OutlinedTextFields.propTypes = {
 
 export default OutlinedTextFields;
 
-// import React from 'react';
-// import './style.css';
-
-// class AddItemForm extends React.Component {
-//     // constructor(props) {
-//     //     super(props);
-//         nameRef = React.createRef();
-//         priceRef = React.createRef();
-//         linkRef = React.createRef();
-//         descRef = React.createRef();
-//         imageRef = React.createRef();
-//     // }
-
-//     createItem = (event) => {
-//         event.preventDefault();
-//         const item = {
-//             name: this.nameRef.current.value,
-//             price: this.priceRef.current.value,
-//             link: this.linkRef.current.value,
-//             desc: this.descRef.current.value,
-//             image: this.imageRef.current.value
-//         };
-//         console.log(item);
-//     };
-//     render() {
-//         return (
-//             <div className="form-container">
-//             <form className="add-item" onSubmit={this.createItem}>
-//                 <input name ="name" ref={this.nameRef} type="text" placeholder="name" />
-//                 <input name ="price" ref={this.priceRef} type="text" placeholder="price" />
-//                 <input name ="link" ref={this.linkRef} type="text" placeholder="link" />
-//                 <textarea name ="desc" ref={this.descRef} placeholder="desc"></textarea>
-//                 <input name ="image" ref={this.imageRef} type="text" placeholder="image" />
-//                 <button type="submit" onClick={this.createItem}>+Item</button>
-//             </form>
-//             </div>
-//         );
-//     }
-// }
-
-// export default AddItemForm;
